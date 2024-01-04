e6709f5d13e168837960589f2f24a500cf0fc6d1
Sujoy De <sujoy.de345@gmail.com>
2024-01-04 10:34:42 +0530

    Add mocks for local testing

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,9 +2,13 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
 contract HelperConfig is Script {
     NetworkConfig public networkConfig;
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_ANSWER = 2000e8;
 
     struct NetworkConfig {
         address priceFeed;
@@ -34,7 +38,17 @@ contract HelperConfig is Script {
             });
     }
 
-    function getAnvilConfig() public pure returns (NetworkConfig memory) {
-       
+    function getAnvilConfig() public returns (NetworkConfig memory) {
+        if(networkConfig.priceFeed != address(0)) {
+            return networkConfig;
+        }
+
+        vm.startBroadcast();
+        MockV3Aggregator mockV3Aggregator = new MockV3Aggregator(
+            DECIMALS,
+            INITIAL_ANSWER
+        );
+        vm.stopBroadcast();
+        return NetworkConfig({priceFeed: address(mockV3Aggregator)});
     }
 }
